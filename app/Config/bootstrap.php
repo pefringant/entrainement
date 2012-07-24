3691be99886940c0c7436d39d697774c8bed535b
Pierre-Emmanuel Fringant <pierre.fringant@gmail.com>
2012-07-24 13:03:56 +0200

    Installation et initialisation plugin Migration

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -179,3 +179,8 @@ CakeLog::config('error', array(
 	'types' => array('warning', 'error', 'critical', 'alert', 'emergency'),
 	'file' => 'error',
 ));
+
+/**
+ * Loading plugins
+ */
+CakePlugin::load('Migrations');
